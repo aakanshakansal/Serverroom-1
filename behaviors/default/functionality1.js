085a6ac405cb4bbde44dd902c86ecf959a4d4703
aakanshakansal01 <aakanshakansal5@gmail.com>
2024-05-27 14:09:34 +0530

    seventh commit

--- behaviors/default/functionality1.js
+++ behaviors/default/functionality1.js
@@ -25,30 +25,6 @@ class ModelPawn extends PawnBehavior {
         // let cfd=[];
         // let temp=[];
         
-        // const loadModelPromise = new Promise((resolve, reject) => {
-        //     gltfLoader.load(
-        //         './assets/Server_Room_New_NEW 1wire blend.glb', 
-        //         (gltf) => {
-        //             const model = gltf.scene;
-        
-        //             model.position.set(0, -1.6, 0);
-        //             const scaleFactor = 8;
-        //             model.scale.set(scaleFactor, scaleFactor, scaleFactor);
-        
-        //             group.add(model);
-        //             console.log(model);
-        
-        //             resolve(model);
-        //         },
-        //         null,
-        //         (error) => {
-        //             console.error('Error loading GLTF model:', error);
-        //             reject(error);
-        //         }
-        //     );
-        // });
-
-
         const loadModelPromise = new Promise((resolve, reject) => {
             gltfLoader.load(
                 './assets/Server_Room_New_NEW 1wire blend.glb', 
@@ -64,15 +40,39 @@ class ModelPawn extends PawnBehavior {
         
                     resolve(model);
                 },
-                (xhr) => {
-                    console.log(`${(xhr.loaded / xhr.total * 100)}% loaded`);
-                },
+                null,
                 (error) => {
                     console.error('Error loading GLTF model:', error);
                     reject(error);
                 }
             );
         });
+
+
+        // const loadModelPromise = new Promise((resolve, reject) => {
+        //     gltfLoader.load(
+        //         './assets/Server_Room_New_NEW 1wire blend.glb', 
+        //         (gltf) => {
+        //             const model = gltf.scene;
+        
+        //             model.position.set(0, -1.6, 0);
+        //             const scaleFactor = 8;
+        //             model.scale.set(scaleFactor, scaleFactor, scaleFactor);
+        
+        //             group.add(model);
+        //             console.log(model);
+        
+        //             resolve(model);
+        //         },
+        //         (xhr) => {
+        //             console.log(`${(xhr.loaded / xhr.total * 100)}% loaded`);
+        //         },
+        //         (error) => {
+        //             console.error('Error loading GLTF model:', error);
+        //             reject(error);
+        //         }
+        //     );
+        // });
         
 
         loadModelPromise.then((model) => {
